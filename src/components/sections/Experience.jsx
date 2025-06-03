81c5805855897e3f74f15a4c6be5701a68b604b0
Cesar Aguilar <cesarandre940@gmail.com>
2025-06-03 16:20:45 -0600

    feat: Update experience descriptions for clarity and detail

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -8,7 +8,7 @@ const EXPERIENCE = [
     title: "Frontend Developer ",
     company: "McSystems",
     description:
-      "Built a financial core system from scratch, automating key processes and enhancing efficiency. Developed an AI-driven chatbot, improving IT support response time by 33%. Automated check printing, reducing processing time by 30%. Implemented secure role-based authentication to protect sensitive financial data.",
+      "Designed and developed a cross platform desktop app for a bank using Tauri, TypeScript, and React used daily by employees to manage critical financial operations. Led the creation of secure, role based authentication with branch specific access control, safeguarding sensitive data and tailoring user experience. Launched an AI-powered chatbot and ticketing system, cutting IT response time by 33% and boosting user satisfaction. Automated the check-printing process, improving efficiency by 30% and reducing paper usage. Additionally, mentored junior developers, driving frontend excellence and aligning the team with business goals.",
   },
   {
     date: "October 2023",
@@ -26,19 +26,19 @@ export const Experience = () => {
       className="min-h-screen flex items-center justify-center py-20 bg-black text-gray-100"
     >
       <RevealOnScroll>
-      <div className="max-w-5xl mx-auto px-4">
-        <h2  className="flex items-center justify-center gap-2 text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-        <GlassesIcon/>
-          Experience
-        </h2>
-        <ol className="relative mx-12">
-          {EXPERIENCE.map((experience, index) => (
-            <li key={index}>
-              <ExperienceItem {...experience} />
-            </li>
-          ))}
-        </ol>
-      </div>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="flex items-center justify-center gap-2 text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            <GlassesIcon />
+            Experience
+          </h2>
+          <ol className="relative mx-12">
+            {EXPERIENCE.map((experience, index) => (
+              <li key={index}>
+                <ExperienceItem {...experience} />
+              </li>
+            ))}
+          </ol>
+        </div>
       </RevealOnScroll>
     </section>
   );
